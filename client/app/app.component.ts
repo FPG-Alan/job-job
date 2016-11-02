40c449c5725756e408952691721bfbad841e2ea7
David Thong Nguyen <thnguyen2808@gmail.com>
2016-11-02 16:12:52 -0400

    Missed this in last commit

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {AuthService} from "./services/auth.service";
 
 declare var $;
 
@@ -9,6 +10,9 @@ declare var $;
 })
 export class AppComponent {
     title = 'app works!';
+
+    constructor(private authService: AuthService) {
+    }
 
     toggleSidebar() {
         $(".ui.sidebar").sidebar("toggle");
