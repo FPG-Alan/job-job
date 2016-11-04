da3e65bb34291a912a41025e788d150ad7c77465
David Thong Nguyen <thnguyen2808@gmail.com>
2016-11-04 16:42:06 -0400

    Setting up form UI

--- client/app/modules/create-job-form/create-job-form.component.ts
+++ client/app/modules/create-job-form/create-job-form.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import {NgForm} from "@angular/forms";
+import {Job} from "../../classes/job";
 
 @Component({
     selector: 'app-create-job-form',
@@ -6,7 +8,8 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ['./create-job-form.component.scss']
 })
 export class CreateJobFormComponent implements OnInit {
-    
+
+    job = new Job("", null, "", null, "", "", "", "", null, "", "", []);
 
     constructor() {
     }
@@ -14,4 +17,8 @@ export class CreateJobFormComponent implements OnInit {
     ngOnInit() {
     }
 
+    onSubmit(form: NgForm) {
+        console.log(form.value);
+        console.log(form.valid)
+    }
 }
