66985f5bce80f3409770f17c5bb3ddf7a354cc41
David Thong Nguyen <thnguyen2808@gmail.com>
2016-11-28 13:52:32 -0500

    Display table can sort

--- client/app/components/display-jobs/display-jobs.component.ts
+++ client/app/components/display-jobs/display-jobs.component.ts
@@ -16,6 +16,8 @@ export class DisplayJobsComponent implements OnInit {
     displayedJobs: any[] = [];
     clients: Client[] = [];
     loading: boolean = false;
+    sortOrder: string = "";
+    sortCategory: string = "";
 
     constructor(private apiService: ApiService,
                 private commonService: CommonService) {
@@ -30,6 +32,7 @@ export class DisplayJobsComponent implements OnInit {
                 result => {
                     this.jobs = result.data;
                     this.onFilterSearchChange(""); // TODO: can be stored in localStorage
+                    this.sortTable("name");
                     console.log(this.jobs);
                 },
                 err => this.commonService.handleError(err),
@@ -42,9 +45,9 @@ export class DisplayJobsComponent implements OnInit {
             );
     }
 
-    onFilterSearchChange(searchValues: string){
+    onFilterSearchChange(searchValues: string) {
         this.loading = true;
-        if (!this.commonService.isEmptyString(searchValues)){
+        if (!this.commonService.isEmptyString(searchValues)) {
             let filteredClients = searchValues.split(",");
             console.log("loading", filteredClients);
 
@@ -59,5 +62,52 @@ export class DisplayJobsComponent implements OnInit {
             this.displayedJobs = this.jobs;
         }
         this.loading = false;
+    }
+
+    sortTable(category: string) {
+        if (this.sortCategory != category) {
+            // ascending order if not sorted
+            this.sortOrder = "descending";
+            this.sortCategory = category;
+        } else {
+            // toggle if already sorted
+            if (this.sortOrder == "descending") {
+                this.sortOrder = "ascending";
+            } else if (this.sortOrder == "ascending") {
+                this.sortOrder = "descending"
+            }
+        }
+
+        this.displayedJobs.sort((a: any, b: any) => {
+            if (this.sortCategory == "name") {
+                if (this.sortOrder == "descending") {
+                    return this.compareString(a.name, b.name);
+                } else {
+                    return this.compareString(b.name, a.name);
+                }
+            } else if (this.sortCategory == "client") {
+                if (this.sortOrder == "descending") {
+                    return this.compareString(a.client, b.client);
+                } else {
+                    return this.compareString(b.client, a.client);
+                }
+            }
+        });
+    }
+
+    compareString(a: string, b: string) {
+        if (!a) a = "";
+        if (!b) b = "";
+        // compare to see if the 1st string is larger than the 2nd
+        if (a < b) {
+            console.log(a, b, "returned -1");
+            return -1;
+        } else if (a > b) {
+            console.log(a, b, "returned 1");
+            return 1;
+        } else {
+            console.log(a, b, "returned 0");
+            return 0;
+        }
     }
 }
