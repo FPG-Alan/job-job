25703036c2174f2550a54135ffcae4f8c796f00c
David Thong Nguyen <thnguyen2808@gmail.com>
2016-11-30 18:48:33 -0500

    Code cleanup

--- server/routers/rateCardRouter.js
+++ server/routers/rateCardRouter.js
@@ -12,32 +12,6 @@ var tenKApiKeys = {
     }
 };
 
-rateCardRouter.get("/roles", function (req, res) {
-    unirest.post(tenKApiKeys.dev.url + "roles?per_page=100000")
-        .headers({
-            "Content-Type": "application/json",
-            "auth": tenKApiKeys.dev.keys
-        })
-        .end(function (response) {
-            // TODO: handle err
-            console.log("All roles:", response.body);
-            res.send(response.body);
-        });
-});
-
-rateCardRouter.get("/disciplines", function (req, res) {
-    unirest.post(tenKApiKeys.dev.url + "disciplines?per_page=100000")
-        .headers({
-            "Content-Type": "application/json",
-            "auth": tenKApiKeys.dev.keys
-        })
-        .end(function (response) {
-            // TODO: handle err
-            console.log("All disciplines:", response.body);
-            res.send(response.body);
-        });
-});
-
 rateCardRouter.get("/:project", function (req, res) {
     unirest.get(tenKApiKeys.dev.url + "projects/" + req.params.project + "/bill_rates" +
         "?per_page=100000")
