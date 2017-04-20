31a05167a4bbb541f88ecc15d98b0c275a3b0deb
David Thong Nguyen <thnguyen2808@gmail.com>
2017-04-20 13:47:43 -0400

    Fix route not redirected when not logged in

--- client/app/services/auth.guard.ts
+++ client/app/services/auth.guard.ts
@@ -12,6 +12,7 @@ export class AuthGuard implements CanActivate {
         if (this.authService.authenticated()) {
             return true;
         } else {
+            this.authService.logout();
             return false;
         }
     }
